5d4bec411260b28d89a81c04b52b6bf0150fc2df
Victor Pizetta <joao.victor07.br2@gmail.com>
2022-03-14 21:28:49 -0300

    Initial Commit

---
+++ Aula06_Table_CollectionView_homework/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Aula06_Table_CollectionView_homework
+//
+//  Created by Victor Pizetta on 14/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
